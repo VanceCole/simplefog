5839d9a596c108b5abf971f243e718360097e35c
Vance Cole <coletain@gmail.com>
2020-07-04 01:24:10 -0700

    adds placeholder buttons for controls

--- js/ui.js
+++ js/ui.js
@@ -8,6 +8,51 @@ Hooks.on("getSceneControlButtons", (controls) => {
         layer: "SimpleFogLayer",
         tools: [
           {
+            name: "brush",
+            title: "Brush Tool",
+            icon: "fas fa-paint-brush",
+            onClick: () => {
+              console.log('Brush');
+            },
+            button: true
+          },
+          {
+            name: "shape",
+            title: "Shape Tool",
+            icon: "far fa-star",
+            onClick: () => {
+              console.log('Shape');
+            },
+            button: true
+          },
+          {
+            name: "box",
+            title: "Box Tool",
+            icon: "far fa-square",
+            onClick: () => {
+              console.log('Box');
+            },
+            button: true
+          },
+          {
+            name: "circle",
+            title: "Circle Tool",
+            icon: "far fa-circle",
+            onClick: () => {
+              console.log('Circle');
+            },
+            button: true
+          },
+          {
+            name: "settings",
+            title: "Settings",
+            icon: "fa fa-cog",
+            onClick: () => {
+              console.log('Settings');
+            },
+            button: true
+          },
+          {
             name: "clearfog",
             title: "CONTROLS.ClearFog",
             icon: "fas fa-trash",
