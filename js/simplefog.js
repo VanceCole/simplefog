f67ef6f771ad01a2bade640766e1585361cd60a0
Jeremy <jeremy@uranusbytes.com>
2022-06-15 20:44:16 -0400

    Move HUD Controls into a new layer directly below simplefog #65

--- js/simplefog.js
+++ js/simplefog.js
@@ -1,8 +1,8 @@
 import SimplefogLayer from '../classes/SimplefogLayer.js';
-import sightLayerUpdate from './sightLayerUpdate.js';
 import SimplefogMigrations from '../classes/SimplefogMigrations.js';
 import config from './config.js';
 import { simplefogLog } from './helpers.js';
+import SimplefogHUDControlLayer from "../classes/SimplefogHUDControlLayer.js";
 
 Hooks.once('init', () => {
   // eslint-disable-next-line no-console
@@ -18,7 +18,11 @@ Hooks.once('canvasInit', () => {
   if (isNewerVersion(game.version, "9")) {
     CONFIG.Canvas.layers["simplefog"] = {
       layerClass: SimplefogLayer,
-      group: "effects"
+      group: "primary"
+    };
+    CONFIG.Canvas.layers["simplefogHUDControls"] = {
+      layerClass: SimplefogHUDControlLayer,
+      group: "primary"
     };
     Object.defineProperty(canvas, 'simplefog', {
       value: new SimplefogLayer(),
@@ -26,13 +30,23 @@ Hooks.once('canvasInit', () => {
       writable: true,
       enumerable: false,
     });
+    Object.defineProperty(canvas, 'simplefogHUDControls', {
+      value: new SimplefogHUDControlLayer(),
+      configurable: true,
+      writable: true,
+      enumerable: false,
+    });
     canvas.primary.addChild(canvas.simplefog);
+    canvas.primary.addChild(canvas.simplefogHUDControls);
   } else {
     canvas.simplefog = new SimplefogLayer();
     canvas.stage.addChild(canvas.simplefog);
+    canvas.simplefogHUDControls = new simplefogHUDControls();
+    canvas.stage.addChild(canvas.simplefogHUDControls);
 
     let theLayers = Canvas.layers;
     theLayers.simplefog = SimplefogLayer;
+    theLayers.simplefogHUDControls = SimplefogHUDControlLayer;
     Object.defineProperty(Canvas, 'layers', {get: function() {
         return theLayers
     }})
@@ -48,9 +62,10 @@ Hooks.once('ready', () => {
 
   // Fix simplefog zIndex
   canvas.simplefog.zIndex = canvas.simplefog.getSetting('layerZindex');
+  canvas.simplefogHUDControls.zIndex = canvas.simplefog.getSetting('layerZindex') - 1;
 
   // // Move object hud to tokens layer
-  game.canvas.controls.hud.setParent(game.canvas.tokens)
+  game.canvas.controls.hud.setParent(game.canvas.simplefogHUDControls)
 
   //ooks.on('sightRefresh', sightLayerUpdate);
   canvas.sight.refresh();
